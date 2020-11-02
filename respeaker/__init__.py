b2fc44b889519a6b8c4ec86de8a2253fe59ccd30
Marvin Wichmann <marvin.wichmann@unic.com>
2020-11-02 19:45:51 +0100

    Only import strategies when we actually need them as it saves some time

--- respeaker/__init__.py
+++ respeaker/__init__.py
@@ -1,9 +1,6 @@
 """ReSpeaker library."""
 import subprocess
 
-from .corev2 import CoreV2
-from .fourmic import Respeaker4Mic
-from .sixmic import Respeaker6Mic
 from .strategy import RespeakerStrategy
 
 
@@ -13,13 +10,16 @@ def obtain_strategy(pattern, bus) -> RespeakerStrategy:
     if '2mic' in result:
         raise NotImplementedError
     if '4mic' in result:
+        from .fourmic import Respeaker4Mic
         return Respeaker4Mic(pattern, bus)
     if '6mic' in result:
+        from .sixmic import Respeaker6Mic
         return Respeaker6Mic(pattern, bus)
 
     result = subprocess.run(['uname', '-a'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
     if 'respeaker' in result:
+        from .corev2 import CoreV2
         return CoreV2(pattern, bus)
 
     #  TODO: We should catch this and speak a dialog to the user in this case.
